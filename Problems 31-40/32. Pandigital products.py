9a3373c568cafcb7676dcc2587c23ffd369dba02
seyong2 <41242974+seyong2@users.noreply.github.com>
2022-07-02 15:22:58 +0200

    Add files via upload

--- Problems 31-40/32. Pandigital products.py
+++ Problems 31-40/32. Pandigital products.py
@@ -1,16 +1,32 @@
 import time as time
 from itertools import permutations
+
+def perm_n(l, n):
+    if n == 0:
+        return [[]]
+    else:
+        perms = []
+        for i in range(0, len(l)):
+            temp = l[:]
+            a = temp[i]
+            temp.remove(a)
+            remain_combi = perm_n(temp, n-1)
+            for j in remain_combi:
+                perms.append((a, *j))
+        return perms
+    
 tic = time.time()
 
 result = []
+
 for i in range(1, 10):
-    for j in permutations(list(range(1, 10)), i):
+    for j in perm_n(list(range(1, 10)), i):
         a = ""
         for k in j:
             a += str(k)
         a = int(a)
         for k in range(1, 10-len(j)):
-            for l in permutations([item for item in list(range(1, 10)) if item not in j], k):
+            for l in perm_n([item for item in list(range(1, 10)) if item not in j], k):
                 b = ""
                 for m in l:
                     b += str(m)
@@ -21,7 +37,5 @@ for i in range(1, 10):
                         result.append(product)
 
 toc = time.time()
-print(sum(result))
+print(sum(result)) # 45228
 print(toc-tic)     
-        
-        
