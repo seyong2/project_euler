4a7731ef157df4bf6f8110486796ec21ef7e47f2
seyong2 <41242974+seyong2@users.noreply.github.com>
2022-07-14 08:24:02 +0200

    Add files via upload

--- Problems 31-40/36. Double-base palindromes.py
+++ Problems 31-40/36. Double-base palindromes.py
@@ -1,28 +1,30 @@
 import time as time
-start = time.time()
+
 def palindrome_10(number):
-    if number==int(str(number)[::-1]):
+    if number == int(str(number)[::-1]):
         return True
     else:
         return False
 
 def palindrome_2(number):
     remainders = ''
-    while number>0:
+    while number > 0:
         remainders += str(number%2)
-        number = number//2
-    if int(remainders)==int(remainders[::-1]):
+        number //= 2
+    if remainders == remainders[::-1]:
         return True
     else:
         return False
 
+tic = time.time()
+
 solution = 0
-for n in range(1,10**6):
-    if palindrome_10(n)+palindrome_2(n)==2:
-        solution += n
-    else:
-        continue
-end = time.time()
+for n in range(1, 10**6):
+    if palindrome_10(n):
+        if palindrome_2(n):
+            solution += n
+
+toc = time.time()
 print(solution)
-print(end-start)
+print(toc - tic)
         
