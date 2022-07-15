33b7e2264ce0fc60bcd40bbeab304daa7b79bbfb
seyong2 <41242974+seyong2@users.noreply.github.com>
2022-07-15 12:51:22 +0200

    Add files via upload

--- Problems 31-40/37. Truncatable primes.py
+++ Problems 31-40/37. Truncatable primes.py
@@ -1,7 +1,6 @@
 import time as time
-tic = time.time()
 
-def prime(number):
+def is_prime(number):
     if number in [0,1]:
         return False
     elif number in [2,3]:
@@ -9,31 +8,27 @@ def prime(number):
     elif number%2 == 0:
         return False
     else:
-        division = 0
         for d in range(2, int(number**0.5)+1):
             if number%d == 0:
-                division += 1
-            else:
-                continue
-        if division == 0:
-            return True
-        else:
-            return False
+                return False
+                break
+        return True
+
+tic = time.time()
 
 n = 23
 truncatable_primes = list()  
 
-while len(truncatable_primes)<11:
-    if prime(n):
+while len(truncatable_primes) < 11:
+    if is_prime(n):
         if (int(str(n)[0]) in [2, 3, 5, 7]) and (int(str(n)[-1]) in [3, 7]):
             count = 0
             for i in range(1, len(str(n))):
-                if prime(int(str(n/(10**i)).split('.')[0])) and prime(int(str(n/(10**i)).split('.')[-1])):
+                if is_prime(n%(10**i)) and is_prime(n//(10**i)):
                     count += 1
             if count == len(str(n))-1:
                 truncatable_primes.append(n)
-    n += 2
-                
+    n += 2                
     
 toc = time.time()
 print(sum(truncatable_primes))
