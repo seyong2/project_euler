440c0295452c7200c99d08a4f1670fb9461d983d
seyong2 <41242974+seyong2@users.noreply.github.com>
2022-07-17 09:42:06 +0200

    Add files via upload

--- Problems 31-40/40. Champernowne's constant.py
+++ Problems 31-40/40. Champernowne's constant.py
@@ -1,20 +1,23 @@
 import time as time
-start = time.time()
+tic = time.time()
 
 # create Champernowne's constant
+# we are interested in only some of the fractional parts indicated below
+digit_list = [10**0, 10**1, 10**2, 10**3, 10**4, 10**5, 10**6]
+solution = 1
 fraction = ''
 i = 1
 while len(fraction) < 10**6:
     fraction += str(i)
+    for j in digit_list:
+        if len(fraction) >= j:
+            solution *= int(fraction[j-1])
+            digit_list.remove(j)
+        else:
+            break
     i += 1
 
-# we are interested in only some of the fractional parts indicated below
-digit_list = [10**0, 10**1, 10**2, 10**3, 10**4, 10**5, 10**6]
-solution = 1
-for digit in digit_list:
-    solution *= int(fraction[digit-1])
-
-end = time.time()
+toc = time.time()
 print(solution)
-print(end - start)
+print(toc - tic)
     
