7ab4734c23cd31aa19863e5054ad4901ebe15ca7
seyong2 <41242974+seyong2@users.noreply.github.com>
2022-07-18 10:12:22 +0200

    Add files via upload

--- Problems 41-50/42. Coded triangle numbers.py
+++ Problems 41-50/42. Coded triangle numbers.py
@@ -1,27 +1,27 @@
-import csv
+import string
 import time as time
-start = time.time()
+
+alphabets = list(string.ascii_uppercase)
+
+with open('p042_words.txt') as f:
+    data = f.read().strip('"').split('","')
+
+def is_int_zero_of_function(number):
+    zero_of_function = (-1+(1+4*2*number)**0.5)/2
+    if str(zero_of_function).split('.')[-1] == '0':
+        return True
+
+tic = time.time()
 
 solution = 0
-alphabets = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11,
-             'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21,
-             'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
-with open('p042_words.txt') as f:
-    for words in csv.reader(f):
-        for word in words:
-            word_value = 0
-            for character in word:
-                word_value += alphabets[character]
-            n = 1
-            while True:
-                triangle_number = 0.5*n*(n+1)
-                if triangle_number == word_value:
-                    solution += 1
-                elif triangle_number > word_value:
-                    break
-                n += 1
-                
-end = time.time()
-print(solution)
-print(end - start)              
 
+for word in data:
+    word_value = 0
+    for character in word:
+        word_value += alphabets.index(character)+1
+    if is_int_zero_of_function(word_value):
+        solution += 1
+                
+toc = time.time()
+print(solution)
+print(toc - tic)              
--- Problems 41-50/43. Sub-string divisibility.py
+++ Problems 41-50/43. Sub-string divisibility.py
@@ -1,22 +1,41 @@
 import time as time
-from itertools import permutations
-tic = time.time()
+
+def perm_n(l, n):
+    if n == 0:
+        return [[]]
+    # when we want more than zero element
+    else:
+        # we create an empty list that we will append all possible permutations of n elements
+        perms = []
+        # iterate through the elements of the list
+        for i in range(0, len(l)):
+            temp = l[:] # copy
+            # take one element
+            a = temp[i]
+            # the remaining elements of the list 
+            temp.remove(a)
+            # we find permutations that have n-1 elements
+            remain_combi = perm_n(temp, n-1)
+            for j in remain_combi:
+                # * is for getting rid of parenthesis ()
+                perms.append((a, *j))
+        return perms
 
 result = 0
-digits = list(range(1, 10))
+digits = list(str(s) for s in range(0, 10))
 div = {1:2, 2:3, 3:5, 4:7, 5:11, 6:13, 7:17}
-for i in list(permutations(digits, len(digits))):
-    number = ''
-    for j in i:
-        number += str(j)
-    for k in range(1, 10):
-        number_temp = number[:k] + str(0) + number[k:]
+
+tic = time.time()
+
+for i in perm_n(digits, len(digits)):
+    if i[0] != '0':
+        number = ''.join(i)
         count = 0
         for m, n in div.items():
-            if int(number_temp[m:m+3])%n == 0:
+            if int(number[m:m+3])%n == 0:
                 count += 1
         if count == len(div):
-            result += int(number_temp)
+            result += int(number)
         
 toc = time.time()
 print(result)
