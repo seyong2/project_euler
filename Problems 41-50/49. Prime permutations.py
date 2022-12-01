1b55b8dd28e6350efbf82372b93d9f91dc7af5c5
seyong2 <41242974+seyong2@users.noreply.github.com>
2022-12-01 22:56:22 +0100

    Add files via upload

--- Problems 41-50/49. Prime permutations.py
+++ Problems 41-50/49. Prime permutations.py
@@ -1,14 +1,13 @@
 import time as time
-import numpy as np
 
 def is_prime(n):
-    if (n in [0, 1]) or (n%2 == 0):
+    if n <= 3:
+        return n > 1
+    elif n%2 == 0 or n%3 == 0:
         return False
-    elif n in [2, 3]:
-        return True
     else:
-        for d in range(2, int(n**0.5)+1):
-            if n%d == 0:
+        for d in range(5, int(n**0.5)+1, 6):
+            if n%d == 0 or n%(d+2) == 0:
                 return False
                 break
         return True
@@ -37,41 +36,40 @@ def combi_n(l, n):
             remain = l[i+1:]
             remain_combi = combi_n(remain, n-1)
             for j in remain_combi:
-                combis.append((a, *j))
+                combis.append([a, *j])
         return combis
     
 tic = time.time()
 
-results = list()
+result = 0
+i = 1848
 
-for i in range(1000, 10000):
-    result = ''
+while result == 0:
     # check whether the number is prime or not
     if is_prime(i) == True:
         # if it's prime, create an empty list
         primes = list()
         # make a list of permutations using the digits of the number
         for j in perm_n(list(str(i)), 4):
-            # we check whether the digit '0' comes first (we don't want 0123)
+            # we check whether the digit '0' comes first (i.e., we don't want 0123)
             # we also don't want the last digit is '0' because it's not prime.
-            if (j[0] != '0') and (j[-1] != '0'):
+            if '0' not in [j[0], j[-1]]:
                 # we join the digits altogether
-                number = ''.join(j)
-                # check whether the joined number is prime or not
-                # if it's prime, we include that number in the list
-                # the second condition is for the cases where there are two same digits
-                if (is_prime(int(number))) and (int(number) not in primes):
-                    primes.append(int(number))
+                number = int(''.join(j))
+                # check whether the joined number is prime or not.
+                # if it's prime, we include that number in the list.
+                # the second condition is for the cases where there are two same digits.
+                if (is_prime(number)) and (number not in primes):
+                    primes.append(number)
         for l in combi_n(primes, 3):
-            l = sorted(l)
+            l.sort()
             if l[1] == (l[0]+l[2])/2:
-                for m in l:
-                    result += str(m)
-                if int(result) not in results:
-                    results.append(int(result))                
+                result = int(''.join(map(str, l)))
+                break
+    i += 1
 
 toc = time.time()
-print(results)
+print(result)
 print(toc - tic)
                         
                     
