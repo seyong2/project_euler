389042c3f4971b7c2cc500def2e49620f7acc1a9
seyong2 <41242974+seyong2@users.noreply.github.com>
2022-12-02 11:41:51 +0100

    Add files via upload

--- Problems 41-50/50. Consecutive prime sum.py
+++ Problems 41-50/50. Consecutive prime sum.py
@@ -1,42 +1,41 @@
 import time as time
 
 def is_prime(n):
-    if n in [0, 1]:
-        return False
-    elif n in [2, 3]:
-        return True
-    elif n%2 == 0:
+    if n <= 3:
+        return n > 1
+    elif n%2 == 0 or n%3 == 0:
         return False
     else:
-        for i in range(2, int(n**(1/2))+1):
+        for i in range(5, int(n**(1/2))+1):
             if n%i == 0:
                 return False
                 break
-            else:
-                continue
         return True
 
 tic = time.time()
 
-primes = [2]
-results = []
+primes = []
+cond = True
+result = 0
 
-n = 3
-while True:
+n = 1
+
+while cond:
     if is_prime(n):
         primes.append(n)
-        for i in range(0, len(primes)):
+        i = 0
+        while i < len(primes):
             sum_primes = sum(primes[i:])
-            if (is_prime(sum_primes)) and (sum_primes not in results):
-                results.append(sum_primes)
-                break
-        if max(sorted(results))>10**6:
-            break
-       
+            if (is_prime(sum_primes)) and (sum_primes > result):
+                if sum_primes < 10**6:
+                    result = sum_primes
+                else:
+                    cond = False
+                    break
+            i += 1           
     n += 1
 
-#results.sort()
-
 toc = time.time()
-print(sorted(results)[-2])
+
+print(result)
 print(toc-tic)
