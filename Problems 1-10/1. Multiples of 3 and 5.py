160a9650ffad6fa42751b0881763da654283ba86
seyong2 <41242974+seyong2@users.noreply.github.com>
2024-11-06 10:24:59 +0100

    Add files via upload

--- Problems 1-10/1. Multiples of 3 and 5.py
+++ Problems 1-10/1. Multiples of 3 and 5.py
@@ -1,16 +1,38 @@
 import time as time
 
+def sum_multiples(n, limit):
+    k = (limit-1)//n
+    return n*k*(k+1)//2
+
 start = time.time()
 
-l = []
+# method 1
+# print(sum([i for i in range(1, 1000) if i%3==0 or i%5==0]))
 
-for i in range(1,1000):
-    if i%3 == 0 or i%5 == 0:
-        l.append(i)
-sum = 0
-for j in l:
-    sum += j
-print(sum)
+# method 2
+# sum_mult_3 = 0
+# i = 1
+# while i <= 1000//3:
+#     sum_mult_3 += 3*i
+#     i += 1
+
+# sum_mult_5 = 0
+# i = 1
+# while i < 1000//5:
+#     sum_mult_5 += 5*i
+#     i += 1
+
+# sum_mult_15 = 0
+# i = 1
+# while i <= 1000//15:
+#     sum_mult_15 += 15*i
+#     i += 1
+
+# print(sum_mult_3 + sum_mult_5 - sum_mult_15)
+
+# method 3
+n = 1000
+print(sum_multiples(3, n) + sum_multiples(5, n) - sum_multiples(15, n))
 
 end = time.time()
 print(end - start)
