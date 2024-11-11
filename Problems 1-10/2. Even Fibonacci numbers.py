77f02c65e97dbade0c023842c964a8ce61f8ba04
seyong2 <41242974+seyong2@users.noreply.github.com>
2024-11-11 10:47:26 +0100

    Add files via upload

--- Problems 1-10/2. Even Fibonacci numbers.py
+++ Problems 1-10/2. Even Fibonacci numbers.py
@@ -1,19 +1,16 @@
 import time as time
 
 start = time.time()
-fib = [1, 2]
-i = 1
-while max(fib) < 4000000:
-    fib.append(fib[i-1]+fib[i])
-    i += 1
-    
-sum = 0
-for j in fib:
-    if j%2 == 0:
-        sum += j
-    else:
-        continue
-print(sum)
-end = time.time()
-print(end-start)
 
+limit = 4*(10**6)
+a, b = 0, 1
+answer = 0
+
+while b < limit:
+    if b % 2 == 0:
+        answer += b
+    a, b = b, a + b
+
+end = time.time()
+print(answer)
+print(end-start)
