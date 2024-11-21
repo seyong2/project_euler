dd5f6d5cb8f4c5f6b58544ece7c7b260addcef2d
seyong2 <41242974+seyong2@users.noreply.github.com>
2024-11-21 10:54:35 +0100

    Add files via upload

--- Problems 1-10/9. Special Pythagorean triplet.py
+++ Problems 1-10/9. Special Pythagorean triplet.py
@@ -1,15 +1,17 @@
 import time as time
 
 start = time.time()
-count = 0
-for a in range(1,501):
-    for b in range(500,500-a,-1):
-        c = 1000 - a - b
-        if c**2 == a**2 + b**2:
-            print(a*b*c)
-            count += 1
-            break
-    if count == 1: break
 
+answer = 1
+for a in range(1, 501):
+    for b in range(500, 500-a, -1):
+        c = 1000-a-b
+        if c**2 == a**2 + b**2:
+            answer = a*b*c
+            break
+    if answer != 1:
+        break
+
+print(answer)
 end = time.time()
 print(end - start)
