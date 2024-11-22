a8c8278db4fedd08d686d1c8e71efd9fea1a22f5
seyong2 <41242974+seyong2@users.noreply.github.com>
2024-11-22 16:30:39 +0100

    Add files via upload

--- Problems 11-20/12. Highly divisible triangular number.py
+++ Problems 11-20/12. Highly divisible triangular number.py
@@ -1,21 +1,49 @@
 import numpy as np
 import time as time
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n%2 == 0 or n%3 == 0:
+        return False
+    else:
+        i = 5
+        while i*i <= n:
+            if n%i == 0 or n%(i+2) == 0:
+                return False
+            i += 6
+        return True
+    
+def prime_factorization(n):
+    prime_factors = []
+    i = 2
+    while n > 1:
+        if is_prime(i):
+            while n%i == 0:
+                prime_factors.append(i)
+                n /= i
+        i += 1
+    return prime_factors
+
+def num_divisors(n):
+    prime_factors = prime_factorization(n)
+    prime_factors = dict((i, prime_factors.count(i)) for i in prime_factors)
+
+    result = 1
+    for i in prime_factors.values():
+        result *= (i+1)
+    return result
+
 start = time.time()
     
-triangle = 0
-natural_number = 1
-while True:
-    triangle += natural_number
-    natural_number += 1
-    divisors = []
-    for i in np.arange(1, np.sqrt(triangle)+1):
-        if (triangle%i == 0) and (i not in divisors):
-            divisors.extend([i, triangle/i])
-        else:
-            pass
-    if len(divisors) > 500:
-        print(triangle)
-        break
-    
+triangle = 1
+i = 1
+while num_divisors(triangle) < 500:
+    i += 1
+    triangle += i
+
+print(triangle)
 end = time.time()
 print(end - start)
